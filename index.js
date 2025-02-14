e8593d63d8c9ca5bdcfeffa5eef505140e45b936
adriancancode <vazqueza2004@gmail.com>
2025-02-14 11:04:09 -0800

    Gameboard layout

---
+++ index.js
@@ -0,0 +1,11 @@
+const gameBoard = document.getElementById("game-board");
+const player = "X";
+function playerTurn() {
+    if
+}
+
+function resetGameboard(parent){
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
